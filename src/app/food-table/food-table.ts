9684ef2ca273eeb2f020ccc40ee73f2afa70baa3
furkan <furkangurbuz5@gmail.com>
2026-07-26 19:44:44 +0200

    Add filters and styling

--- src/app/food-table/food-table.ts
+++ src/app/food-table/food-table.ts
@@ -1,36 +1,70 @@
-import {ChangeDetectionStrategy, Component, computed, inject, Signal, signal, WritableSignal} from '@angular/core';
-import {Meal} from '../interface/food-form.interface';
-import {FoodService} from '../service/food.service';
-import {FormsModule} from '@angular/forms';
-import {FilterSortModel} from '../model/filter-sort.model';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  computed,
+  inject,
+  Signal,
+  signal,
+  WritableSignal,
+} from '@angular/core';
+import { Meal } from '../interface/food-form.interface';
+import { FoodService } from '../service/food.service';
+import { FormsModule } from '@angular/forms';
+import { FilterSortModel } from '../model/filter-sort.model';
+import { debounceTime, distinctUntilChanged, Subject, takeUntil } from 'rxjs';
+import { toSignal } from '@angular/core/rxjs-interop';
 
 @Component({
   selector: 'smoothie-food-table',
-  imports: [
-    FormsModule
-  ],
+  imports: [FormsModule],
   templateUrl: './food-table.html',
   styleUrl: './food-table.scss',
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class FoodTable {
   private readonly foodService: FoodService = inject(FoodService);
 
-  protected ingredients: WritableSignal<Meal[]> = signal([]);
+  private readonly destroy$: Subject<void> = new Subject<void>();
 
-  searchTerm: string = '';
-  initialFilterOn: string = 'ingredient';
+  private ingredients: WritableSignal<Meal[]> = signal([]);
+  private filterTextSubject = new Subject<string>();
+  protected filterText = toSignal(
+    this.filterTextSubject.pipe(
+      takeUntil(this.destroy$),
+      debounceTime(300),
+      distinctUntilChanged(),
+    ),
+    { initialValue: '' },
+  );
+  protected filterType = signal<string>('ingredient');
+  protected filteredIngredients = computed(() => {
+    const text = this.filterText().toLowerCase();
+
+    return this.ingredients().filter((ingredient) => {
+      const value = ingredient['ingredient' as keyof Meal];
+      if (typeof value === 'string') {
+        return value.toLowerCase().includes(text);
+      }
+      return false;
+    });
+  });
 
   ngOnInit() {
     this.ingredients.set(this.foodService.getFoods());
     this.calculateTotals();
   }
 
-  protected onFilterChange(on: string){
-    console.log(on);
+  ngOnDestroy() {
+    this.destroy$.next();
   }
 
-  private calculateTotals(): void {
+  protected updateFilterText(filterText: string): void {
+    this.filterTextSubject.next(filterText);
   }
 
+  protected updateFilterType(filtertype: string): void {
+    this.filterType.set(filtertype);
+  }
+
+  private calculateTotals(): void {}
 }
