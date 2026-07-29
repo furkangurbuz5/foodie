15838bb4514a36e19bb90988207cb95b9a99d945
furkan <furkangurbuz5@gmail.com>
2026-07-29 20:00:24 +0200

    Move filter back to food-table

--- src/app/food-table/food-table.ts
+++ src/app/food-table/food-table.ts
@@ -1,37 +1,23 @@
-import {
-  ChangeDetectionStrategy,
-  Component,
-  computed,
-  inject,
-  Signal,
-  signal,
-  WritableSignal,
-} from '@angular/core';
-import { Food } from '../interface/food-form.interface';
-import { FoodService } from '../service/food.service';
-import { FormsModule } from '@angular/forms';
-import { FoodFilter } from './food-filter/food-filter';
+import {ChangeDetectionStrategy, Component, inject, signal, WritableSignal,} from '@angular/core';
+import {Food} from '../interface/food-form.interface';
+import {FoodService} from '../service/food.service';
+import {FormsModule} from '@angular/forms';
+import {FoodFilterService} from '../filter/meal.filter';
 
 @Component({
   selector: 'smoothie-food-table',
-  imports: [FormsModule, FoodFilter],
+  imports: [FormsModule],
   templateUrl: './food-table.html',
   styleUrl: './food-table.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class FoodTable {
-  private readonly foodService: FoodService = inject(FoodService);
-
   protected foods: WritableSignal<Food[]> = signal([]);
+  private readonly foodService: FoodService = inject(FoodService);
+  private readonly foodFilterService: FoodFilterService = inject(FoodFilterService);
 
   ngOnInit() {
     this.foods.set(this.foodService.getFoods());
   }
 
-  protected updateFoodTable(foods: Food[]) {
-    if (foods) {
-      this.foods.set(foods);
-    }
-    this.foods.set(this.foodService.getFoods());
-  }
 }
