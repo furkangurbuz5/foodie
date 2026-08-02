57a846f6499de59246effba22ad58881010ffdd2
furkan <furkangurbuz5@gmail.com>
2026-08-02 20:42:46 +0200

    remove tomodel

--- backend/foodie/src/main/java/nl/furka/foodie/dto/CreateIngredientRequest.java
+++ backend/foodie/src/main/java/nl/furka/foodie/dto/CreateIngredientRequest.java
@@ -1,13 +1,6 @@
 package nl.furka.foodie.dto;
 
-import nl.furka.foodie.model.Ingredient;
-
 public record CreateIngredientRequest(
   String name,
   int propertiesId
-) {
-
-  public Ingredient toModel(){
-    return null;
-  }
-}
+) {}
